fb04bc089fd2349756c74d8446009243feb02e1a
aleupons <apahoquei@gmail.com>
2021-07-16 18:59:10 +0200

    Collapse searcher i posicionament responsiu per mòbil

--- src/componentes/Searcher/Searcher.js
+++ src/componentes/Searcher/Searcher.js
@@ -1,11 +1,31 @@
 import "./Searcher.css";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { useState } from "react";
 
 export const Searcher = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <aside className="searcher">
-        <form className="row align-items-center justify-content-between">
-          <div className="form-group col-4">
+        {!open && (
+          <button
+            onClick={setOpen(!open)}
+            className="btn"
+            type="button"
+            data-toggle="collapse"
+            data-target="#searcher-form"
+            aria-expanded="false"
+            aria-controls="searcher-form"
+          >
+            {open && <FaBars />}
+            {!open && <FaTimes />}
+          </button>
+        )}
+        <form
+          className="row align-items-center justify-content-between collapse"
+          id="searcher-form"
+        >
+          <div className="form-group col-sm-4">
             <label htmlFor="name">Buscar per nom:</label>
             <input
               type="text"
@@ -14,7 +34,7 @@ export const Searcher = () => {
               placeholder="Introdueix un nom..."
             />
           </div>
-          <div className="form-group d-flex flex-column col-4">
+          <div className="form-group d-flex flex-column col-sm-4">
             <label htmlFor="category">Buscar per categoria:</label>
             <select className="searcher-input form-control" id="category">
               <option value="all" defaultValue>
@@ -25,7 +45,7 @@ export const Searcher = () => {
               <option value="hortalisses">Hortalisses</option>
             </select>
           </div>
-          <div className="form-group d-flex flex-column col-4">
+          <div className="form-group d-flex flex-column col-sm-4">
             <label htmlFor="order">Ordena per:</label>
             <div className="d-flex justify-content-between">
               <select className="searcher-input form-control" id="order">
