c0fda8ba77d503ac82edb71bc4d32b1200c479c6
aleupons <apahoquei@gmail.com>
2021-07-17 15:01:40 +0200

    Classe section no s'ha de posar a la pàgina ShoppingBasket. Ja està a App.js per totes les pàgines del switch del router

--- src/paginas/shoppingBasket/ShoppingBasket.js
+++ src/paginas/shoppingBasket/ShoppingBasket.js
@@ -4,109 +4,107 @@ import product from "../../assets/lettuce-product.jpg";
 
 export const ShoppingBasket = () => {
   return (
-    <div>
-      <section className="section">
-        <div className="header-section mb-6">
-          <h2>Carro de la compra</h2>
-        </div>
-        <table className="table">
-          <thead>
-            <tr>
-              <th className="d-none d-lg-table-cell" scope="col"></th>
-              <th scope="col">Producte</th>
-              <th scope="col">Preu</th>
-              <th scope="col">Quantitat</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="d-none d-lg-block">
-                <img
-                  className="product-img"
-                  src={product}
-                  alt="fruites i verdures de l'hort"
-                />
-              </td>
-              <td className="items-table">Enciam</td>
-              <td className="items-table">1.3€</td>
-              <td className="items-table">
-                <div className="number">
-                  <FaMinus className="icon-counter" />
-                  <input type="text" value="1" readOnly />
-                  <FaPlus className="icon-counter" />
-                </div>
-              </td>
-              <td className="items-table">
-                <FaTimes className="icon-delete" />
-              </td>
-            </tr>
-            <tr>
-              <td className="d-none d-lg-block">
-                <img
-                  className="product-img"
-                  src={product}
-                  alt="fruites i verdures de l'hort"
-                />
-              </td>
-              <td className="items-table">Enciam</td>
-              <td className="items-table">1.3€</td>
-              <td className="items-table">
-                <div className="number">
-                  <FaMinus className="icon-counter" />
-                  <input type="text" value="1" readOnly />
-                  <FaPlus className="icon-counter" />
-                </div>
-              </td>
-              <td className="items-table">
-                <FaTimes className="icon-delete" />
-              </td>
-            </tr>
-            <tr>
-              <td className="d-none d-lg-block">
-                <img
-                  className="product-img"
-                  src={product}
-                  alt="fruites i verdures de l'hort"
-                />
-              </td>
-              <td className="items-table">Enciam</td>
-              <td className="items-table">1.3€</td>
-              <td className="items-table">
-                <div className="number">
-                  <FaMinus className="icon-counter" />
-                  <input type="text" value="1" readOnly />
-                  <FaPlus className="icon-counter" />
-                </div>
-              </td>
-              <td className="items-table">
-                <FaTimes className="icon-delete" />
-              </td>
-            </tr>
-            <tr>
-              <td className="d-none d-lg-block">
-                <img
-                  className="product-img"
-                  src={product}
-                  alt="fruites i verdures de l'hort"
-                />
-              </td>
-              <td className="items-table">Enciam</td>
-              <td className="items-table">1.3€</td>
-              <td className="items-table">
-                <div className="number">
-                  <FaMinus className="icon-counter" />
-                  <input type="text" value="1" readOnly />
-                  <FaPlus className="icon-counter" />
-                </div>
-              </td>
-              <td className="items-table">
-                <FaTimes className="icon-delete" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
-    </div>
+    <section>
+      <div className="header-section mb-6">
+        <h2>Carro de la compra</h2>
+      </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th className="d-none d-lg-table-cell" scope="col"></th>
+            <th scope="col">Producte</th>
+            <th scope="col">Preu</th>
+            <th scope="col">Quantitat</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="d-none d-lg-block">
+              <img
+                className="product-img"
+                src={product}
+                alt="fruites i verdures de l'hort"
+              />
+            </td>
+            <td className="items-table">Enciam</td>
+            <td className="items-table">1.3€</td>
+            <td className="items-table">
+              <div className="number">
+                <FaMinus className="icon-counter" />
+                <input type="text" value="1" readOnly />
+                <FaPlus className="icon-counter" />
+              </div>
+            </td>
+            <td className="items-table">
+              <FaTimes className="icon-delete" />
+            </td>
+          </tr>
+          <tr>
+            <td className="d-none d-lg-block">
+              <img
+                className="product-img"
+                src={product}
+                alt="fruites i verdures de l'hort"
+              />
+            </td>
+            <td className="items-table">Enciam</td>
+            <td className="items-table">1.3€</td>
+            <td className="items-table">
+              <div className="number">
+                <FaMinus className="icon-counter" />
+                <input type="text" value="1" readOnly />
+                <FaPlus className="icon-counter" />
+              </div>
+            </td>
+            <td className="items-table">
+              <FaTimes className="icon-delete" />
+            </td>
+          </tr>
+          <tr>
+            <td className="d-none d-lg-block">
+              <img
+                className="product-img"
+                src={product}
+                alt="fruites i verdures de l'hort"
+              />
+            </td>
+            <td className="items-table">Enciam</td>
+            <td className="items-table">1.3€</td>
+            <td className="items-table">
+              <div className="number">
+                <FaMinus className="icon-counter" />
+                <input type="text" value="1" readOnly />
+                <FaPlus className="icon-counter" />
+              </div>
+            </td>
+            <td className="items-table">
+              <FaTimes className="icon-delete" />
+            </td>
+          </tr>
+          <tr>
+            <td className="d-none d-lg-block">
+              <img
+                className="product-img"
+                src={product}
+                alt="fruites i verdures de l'hort"
+              />
+            </td>
+            <td className="items-table">Enciam</td>
+            <td className="items-table">1.3€</td>
+            <td className="items-table">
+              <div className="number">
+                <FaMinus className="icon-counter" />
+                <input type="text" value="1" readOnly />
+                <FaPlus className="icon-counter" />
+              </div>
+            </td>
+            <td className="items-table">
+              <FaTimes className="icon-delete" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
   );
 };
