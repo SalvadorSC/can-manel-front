9b660a80e3a71ad48d3817fcae0c05affa34bbd1
Pol Reig <83955122+polrb5@users.noreply.github.com>
2021-07-21 19:14:54 +0200

    Método POST implementado en el CRUD de admin

--- src/componentes/AdminForm/AdminForm.js
+++ src/componentes/AdminForm/AdminForm.js
@@ -1,9 +1,56 @@
 import "./AdminForm.css";
 import PropTypes from "prop-types";
 import { FaTimes } from "react-icons/fa";
+import { useState } from "react";
 
 export const AdminForm = (props) => {
   const { action, toggleForm, toggleFormEdit } = props;
+  const [productForm, setProductForm] = useState({
+    name: "",
+    description: "",
+    category: "",
+    priceUnit: "",
+    unit: "",
+    photoUrl: "",
+    discount: "",
+    stock: "",
+    date: Date.now(),
+  });
+
+  const sendForm = async (e) => {
+    e.preventDefault();
+    const product = new FormData();
+    product.append("photoUrl", productForm.photoUrl);
+    product.append("name", productForm.name);
+    product.append("category", productForm.category);
+    product.append("description", productForm.description);
+    product.append("priceUnit", productForm.priceUnit);
+    product.append("unit", productForm.unit);
+    product.append("discount", productForm.discount);
+    product.append("date", productForm.date);
+    product.append("stock", productForm.stock);
+    const resp = await fetch(
+      "https://can-mateu.herokuapp.com/products/new-product/",
+      {
+        method: "POST",
+        body: product,
+      }
+    );
+    if (resp.ok) {
+      return toggleForm();
+    }
+    console.log("Something went wrong");
+  };
+
+  const setData = (e) => {
+    console.log(e.target.files);
+    console.log(e.target.value);
+    setProductForm({
+      ...productForm,
+      [e.target.id]:
+        e.target.type === "file" ? e.target.files[0] : e.target.value,
+    });
+  };
   return (
     <div className="container-create">
       <FaTimes
@@ -16,18 +63,24 @@ export const AdminForm = (props) => {
           }
         }}
       />
-      <form>
+      <form onSubmit={sendForm}>
         <div className="row">
           {action === "create" ? (
             <div className="select-image-create col-3">
-              <label class="file-label" for="file-input">
+              <label className="file-label" htmlFor="photoUrl">
                 Imatge:
               </label>
-              <input type="file" name="attachment[]" multiple="multiple" />
+              <input
+                type="file"
+                id="photoUrl"
+                name="attachment[]"
+                multiple="multiple"
+                onChange={setData}
+              />
             </div>
           ) : (
             <div className="content col-3">
-              <label htmlFor="file-input">
+              <label htmlFor="photoUrl">
                 <img
                   className="content-image"
                   src="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362"
@@ -36,17 +89,22 @@ export const AdminForm = (props) => {
                 <div className="content-details fadeIn-top">
                   <p>Seleccionar imatge</p>
                 </div>
-                <input id="file-input" type="file" />
+                <input id="photoUrl" type="file" onChange={setData} />
               </label>
             </div>
           )}
           <div className="col form-group pt-4">
-            <label htmlFor="productName">Nom producte:</label>
-            <input type="text" className="mb-4 d-block" id="productName" />
-            <label className="col-form-label mr-1" htmlFor="categoryEdit">
+            <label htmlFor="name">Nom producte:</label>
+            <input
+              type="text"
+              className="mb-4 d-block"
+              id="name"
+              onChange={setData}
+            />
+            <label className="col-form-label mr-1" htmlFor="category">
               Categoria:
             </label>
-            <select className="" id="categoryEdit">
+            <select className="" id="category" onChange={setData}>
               <option value="all" defaultValue>
                 Totes les categories
               </option>
@@ -56,30 +114,39 @@ export const AdminForm = (props) => {
             </select>
           </div>
           <div className="col form-group pt-4">
-            <label htmlFor="descriptionProduct">Descripció:</label>
-            <textarea id="descriptionProduct" rows="3" cols="25"></textarea>
+            <label htmlFor="description">Descripció:</label>
+            <textarea
+              id="description"
+              rows="3"
+              cols="25"
+              onChange={setData}
+            ></textarea>
           </div>
         </div>
         <div className="item-price row text-center mt-3">
           <div className="col-3 form-group">
-            <label htmlFor="itemPrice">Preu per unitat:</label>
-            <input type="text" className="" id="itemPrice" />€
+            <label htmlFor="priceUnit">Preu per unitat:</label>
+            <input type="text" id="priceUnit" onChange={setData} />€
           </div>
           <div className="col-3 form-group">
-            <label htmlFor="itemQuantity">Unitat:</label>
-            <input type="text" className="" id="itemQuantity" />€
+            <label htmlFor="unit">Unitat:</label>
+            <input type="text" id="unit" onChange={setData} />
+          </div>
+          <div className="col-3 form-group">
+            <label htmlFor="stock">Stock:</label>
+            <input type="text" id="stock" onChange={setData} />
           </div>
           <div className="col-3 form-group">
             <label htmlFor="discount">Descompte:</label>
-            <input type="text" className="" id="discount" />%
+            <input type="text" id="discount" onChange={setData} />%
           </div>
         </div>
+        <div className="text-right">
+          <button type="submit" className="button" onSubmit={sendForm}>
+            {action === "create" ? "Crear" : "Editar"}
+          </button>
+        </div>
       </form>
-      <div className="text-right">
-        <button type="submit" className="button">
-          {action === "create" ? "Crear" : "Editar"}
-        </button>
-      </div>
     </div>
   );
 };
