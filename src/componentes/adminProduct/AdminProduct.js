195b48b1a413f4e489ad1dc1ad11c3e54348cc21
Pol Reig <83955122+polrb5@users.noreply.github.com>
2021-07-21 13:52:33 +0200

    componente formulario para crear y editar productos

--- src/componentes/adminProduct/AdminProduct.js
+++ src/componentes/adminProduct/AdminProduct.js
@@ -2,19 +2,21 @@ import "./AdminProduct.css";
 import { useState } from "react";
 import PropTypes from "prop-types";
 import { FaEdit, FaImages, FaTimes } from "react-icons/fa";
+import { AdminForm } from "../AdminForm/AdminForm";
 
 export const AdminProduct = (props) => {
   const {
     product: { name, date, category },
+    setAction,
   } = props;
   const [open, setOpen] = useState(false);
   const [formOpen, setFormOpen] = useState(false);
 
+  const toggleFormEdit = () => {
+    setFormOpen(!formOpen);
+  };
   const showImg = () => {
     setOpen(!open);
-  };
-  const toggleForm = () => {
-    setFormOpen(!formOpen);
   };
 
   return (
@@ -46,7 +48,8 @@ export const AdminProduct = (props) => {
           <FaEdit
             className="icon-edit"
             onClick={() => {
-              toggleForm();
+              toggleFormEdit();
+              setAction("edit");
             }}
           />
         </div>
@@ -55,66 +58,7 @@ export const AdminProduct = (props) => {
         </div>
       </div>
 
-      {formOpen && (
-        <div className="container-edit">
-          <FaTimes className="icon-close-form" onClick={toggleForm} />
-          <form>
-            <div className="row">
-              <div className="content col-3">
-                <label htmlFor="file-input">
-                  <img
-                    className="content-image"
-                    src="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362"
-                    alt=""
-                  />
-                  <div className="content-details fadeIn-top">
-                    <p>Seleccionar imatge</p>
-                  </div>
-                  <input id="file-input" type="file" />
-                </label>
-              </div>
-              <div className="col form-group pt-4">
-                <label htmlFor="productName">Nom producte:</label>
-                <input type="text" className="mb-4 d-block" id="productName" />
-                <label className="col-form-label mr-1" htmlFor="categoryEdit">
-                  Categoria:
-                </label>
-                <select className="" id="categoryEdit">
-                  <option value="all" defaultValue>
-                    Totes les categories
-                  </option>
-                  <option value="fruits">Fruites</option>
-                  <option value="vegetables">Verdures</option>
-                  <option value="hortalisses">Hortalisses</option>
-                </select>
-              </div>
-              <div className="col form-group pt-4">
-                <label htmlFor="descriptionProduct">Descripció:</label>
-                <textarea id="descriptionProduct" rows="3" cols="25"></textarea>
-              </div>
-            </div>
-            <div className="item-price row text-center mt-3">
-              <div className="col-3 form-group">
-                <label htmlFor="itemPrice">Preu per unitat:</label>
-                <input type="text" className="" id="itemPrice" />€
-              </div>
-              <div className="col-3 form-group">
-                <label htmlFor="itemQuantity">Unitat:</label>
-                <input type="text" className="" id="itemQuantity" />€
-              </div>
-              <div className="col-3 form-group">
-                <label htmlFor="discount">Descompte:</label>
-                <input type="text" className="" id="discount" />%
-              </div>
-            </div>
-          </form>
-          <div className="text-right">
-            <button type="submit" className="button">
-              Editar
-            </button>
-          </div>
-        </div>
-      )}
+      {formOpen && <AdminForm toggleFormEdit={toggleFormEdit} />}
     </div>
   );
 };
@@ -125,4 +69,5 @@ AdminProduct.propTypes = {
     category: PropTypes.string.isRequired,
     date: PropTypes.string.isRequired,
   }).isRequired,
+  setAction: PropTypes.func.isRequired,
 };
--- src/paginas/adminProductList/AdminProductList.js
+++ src/paginas/adminProductList/AdminProductList.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { FaTimes } from "react-icons/fa";
 import PropTypes from "prop-types";
-import { AdminProduct } from "../../componentes/adminProduct/AdminProduct";
+import { AdminProduct } from "../../componentes/AdminProduct/AdminProduct";
 import "./AdminProductList.css";
+import { AdminForm } from "../../componentes/AdminForm/AdminForm";
 
 export const AdminProductList = (props) => {
   const { products } = props;
@@ -59,74 +59,7 @@ export const AdminProductList = (props) => {
           </button>
         </div>
 
-        {formOpen && (
-          <div className="container-create">
-            <FaTimes className="icon-close-form" onClick={toggleForm} />
-            <form>
-              <div className="row">
-                <div className="content col-3">
-                  <label htmlFor="file-input">
-                    <img
-                      className="content-image"
-                      src="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362"
-                      alt=""
-                    />
-                    <div className="content-details fadeIn-top">
-                      <p>Seleccionar imatge</p>
-                    </div>
-                    <input id="file-input" type="file" />
-                  </label>
-                </div>
-                <div className="col form-group pt-4">
-                  <label htmlFor="productName">Nom producte:</label>
-                  <input
-                    type="text"
-                    className="mb-4 d-block"
-                    id="productName"
-                  />
-                  <label className="col-form-label mr-1" htmlFor="categoryEdit">
-                    Categoria:
-                  </label>
-                  <select className="" id="categoryEdit">
-                    <option value="all" defaultValue>
-                      Totes les categories
-                    </option>
-                    <option value="fruits">Fruites</option>
-                    <option value="vegetables">Verdures</option>
-                    <option value="hortalisses">Hortalisses</option>
-                  </select>
-                </div>
-                <div className="col form-group pt-4">
-                  <label htmlFor="descriptionProduct">Descripció:</label>
-                  <textarea
-                    id="descriptionProduct"
-                    rows="3"
-                    cols="25"
-                  ></textarea>
-                </div>
-              </div>
-              <div className="item-price row text-center mt-3">
-                <div className="col-3 form-group">
-                  <label htmlFor="itemPrice">Preu per unitat:</label>
-                  <input type="text" className="" id="itemPrice" />€
-                </div>
-                <div className="col-3 form-group">
-                  <label htmlFor="itemQuantity">Unitat:</label>
-                  <input type="text" className="" id="itemQuantity" />€
-                </div>
-                <div className="col-3 form-group">
-                  <label htmlFor="discount">Descompte:</label>
-                  <input type="text" className="" id="discount" />%
-                </div>
-              </div>
-            </form>
-            <div className="text-right">
-              <button type="submit" className="button">
-                {action === "create" ? "Crear" : "Editar"}
-              </button>
-            </div>
-          </div>
-        )}
+        {formOpen && <AdminForm action={action} toggleForm={toggleForm} />}
         <div className="table-product row">
           <div className="col-12">
             <div className="table-titles row">
@@ -149,7 +82,11 @@ export const AdminProductList = (props) => {
             </div>
           </div>
           {products.map((product) => (
-            <AdminProduct product={product} key={product._id} />
+            <AdminProduct
+              product={product}
+              key={product._id}
+              setAction={setAction}
+            />
           ))}
         </div>
       </section>
