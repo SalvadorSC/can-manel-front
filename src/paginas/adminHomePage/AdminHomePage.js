0aa3d92ff14fde47501e37f7ab4c471a85be02de
Salvador Sánchez <sanchezcampossalvador@gmail.com>
2021-07-22 10:39:33 +0200

    Links adminsitració i productes destacats limitats a 4

--- src/paginas/adminHomePage/AdminHomePage.js
+++ src/paginas/adminHomePage/AdminHomePage.js
@@ -1,5 +1,6 @@
 import "./AdminHomePage.css";
 import { FaShoppingBasket, FaCrow, FaWarehouse } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export const AdminHomePage = () => {
   const iconsSize = 60;
@@ -10,28 +11,58 @@ export const AdminHomePage = () => {
       <section className="admin">
         <ul className="admin-tasks list-unstyled">
           <li className="task">
-            <FaShoppingBasket size={iconsSize} color={iconsColor} />
-            <span>Productes</span>
+            <Link
+              to="./administracio-productes"
+              className="d-flex flex-column align-items-center"
+            >
+              <FaShoppingBasket size={iconsSize} color={iconsColor} />
+              <span>Productes</span>
+            </Link>
           </li>
           <li className="task">
-            <FaCrow size={iconsSize} color={iconsColor} />
-            <span>Activitats</span>
+            <Link
+              to="./administracio-productes"
+              className="d-flex flex-column align-items-center"
+            >
+              <FaCrow size={iconsSize} color={iconsColor} />
+              <span>Activitats</span>
+            </Link>
           </li>
           <li className="task">
-            <FaWarehouse size={iconsSize} color={iconsColor} />
-            <span>Descomptes</span>
+            <Link
+              to="./administracio-productes"
+              className="d-flex flex-column align-items-center"
+            >
+              <FaWarehouse size={iconsSize} color={iconsColor} />
+              <span>Descomptes</span>
+            </Link>
           </li>
           <li className="task">
-            <FaShoppingBasket size={iconsSize} color={iconsColor} />
-            <span>Productes</span>
+            <Link
+              to="./administracio-productes"
+              className="d-flex flex-column align-items-center"
+            >
+              <FaShoppingBasket size={iconsSize} color={iconsColor} />
+              <span>Productes</span>
+            </Link>
           </li>
           <li className="task">
-            <FaCrow size={iconsSize} color={iconsColor} />
-            <span>Activitats</span>
+            <Link
+              to="./administracio-productes"
+              className="d-flex flex-column align-items-center"
+            >
+              <FaCrow size={iconsSize} color={iconsColor} />
+              <span>Activitats</span>
+            </Link>
           </li>
           <li className="task">
-            <FaWarehouse size={iconsSize} color={iconsColor} />
-            <span>Productes</span>
+            <Link
+              to="./administracio-productes"
+              className="d-flex flex-column align-items-center"
+            >
+              <FaWarehouse size={iconsSize} color={iconsColor} />
+              <span>Productes</span>
+            </Link>
           </li>
         </ul>
       </section>
